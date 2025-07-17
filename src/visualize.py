0bd52671786034b30122731bfc59e517f0a6461b
damius12 <dbettega@unibz.it>
2025-07-17 23:10:01 +0200

    entry features, setup for assignment

--- src/visualize.py
+++ src/visualize.py
@@ -13,6 +13,10 @@ if "data_fetched" not in st.session_state:
     st.session_state.data_fetched = False
 if "control_survey_spec" not in st.session_state:
     st.session_state.control_survey_spec = False
+if "assign_features" not in st.session_state:
+    st.session_state.assign_features = False
+if "skip" not in st.session_state:
+    st.session_state.skip = False
 
 if not st.session_state.data_fetched:
     url = st.text_input("insert winsplit url")
@@ -27,6 +31,7 @@ if not st.session_state.data_fetched:
         )
         st.session_state.data_fetched = True
         st.rerun()
+
 else:
     event_data = st.session_state.raw["event_data"]
     raw = st.session_state.raw["results"]
@@ -39,26 +44,60 @@ else:
         "---"
         "Would you like to add the techical and/or physical features of this race's controls?"
         "You can characterize each control with a custom set of criteria in order to gain a more tailored analysis!"
-        st.subheader("Custom features:")
-        features_names = [1] * st.session_state.features_number
-        for i in range(0, st.session_state.features_number):
-            features_names[i] = st.text_input(
-                f"insert a feature {i}",
-                placeholder="e. g.: climb, vegetation, runnability, tiredness, etc.",
+
+        half, other = st.columns(2)
+        with half:
+            st.subheader("Custom features")
+            features_names = [1] * st.session_state.features_number
+            for i in range(0, st.session_state.features_number):
+                features_names[i] = st.text_input(
+                    f"insert a feature {i}",
+                    placeholder="e. g.: climb, vegetation, runnability, tiredness, etc.",
+                    label_visibility="collapsed",
+                )
+            left, right = st.columns(2)
+            if left.button(
+                "add extra feature", type="secondary", use_container_width=True
+            ):
+                st.session_state.features_number += 1
+                st.rerun()
+            if st.session_state.features_number > 0 and right.button(
+                "remove one feature", type="tertiary", use_container_width=True
+            ):
+                st.session_state.features_number -= 1
+                st.rerun()
+            "you can assign values within a customized range:"
+            feature_range = st.slider(
+                "range",
+                min_value=-10,
+                max_value=10,
+                value=(0, 5),
                 label_visibility="collapsed",
             )
-        left, right = st.columns(2)
-        if left.button("add feature", type="primary", use_container_width=True):
-            st.session_state.features_number += 1
-            st.rerun()
-        if st.session_state.features_number > 0 and right.button(
-            "remove feature", use_container_width=True
-        ):
-            st.session_state.features_number -= 1
-            st.rerun()
+            "---"
+            if "" in features_names:
+                st.warning("to proceed all fields must be filled")
+            yes, no = st.columns(2)
+            if yes.button(
+                "**proceed**",
+                type="primary",
+                use_container_width=True,
+                disabled="" in features_names,
+            ):
+                st.session_state.features = features_names
+                st.session_state.control_survey_spec = True
+                st.rerun()
+            if no.button("skip", use_container_width=True):
+                st.session_state.control_survey_spec = True
+                st.session_state.skip = True
+    if st.session_state.control_survey_spec and not st.session_state.assign_features:
+        st.write(st.session_state.features)
 
-
-if st.session_state.data_fetched and st.session_state.control_survey_spec:
+if (
+    st.session_state.data_fetched
+    and st.session_state.control_survey_spec
+    and (st.session_state.assign_features or st.session_state.skip)
+):
 
     with st.sidebar:
         st.subheader(event_data["name"])
